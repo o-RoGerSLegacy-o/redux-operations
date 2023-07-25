abc8eb3e79d29b6bb558e2e5923912b9f76155f4
o-RoGerSLegacy-o <shubhamhugay@gmail.com>
2023-07-25 23:06:01 +0530

    .

--- src/app/store.js
+++ src/app/store.js
@@ -2,5 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 
 export const store = configureStore({
-  reduce: {},
+  reduce: {
+    counter: counterReducer,
+  },
 });
